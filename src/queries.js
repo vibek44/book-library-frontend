49fa572f93a550c8f20ace7b846bc27ee9c105ed
vibek44 <vibekkshetri@vibeks-MacBook-Pro.local>
2024-04-07 16:03:31 +0300

    adding queries needed by application

---
+++ src/queries.js
@@ -0,0 +1,38 @@
+import { gql } from '@apollo/client'
+export const ALL_AUTHORS = gql`
+query{
+  allAuthors{
+    name
+    born
+    bookCount
+  }
+}`
+
+export const ALL_BOOKS = gql`
+query{
+  allBooks{
+    title
+    author
+    published
+  }
+}`
+
+export const ADD_BOOK = gql`
+  mutation addBook($title: String!, $author: String!,$published:Int!,$genres:[String!]!) {
+    addBook(title: $title, author: $author, published:$published, genres:$genres) {
+      title
+      author
+      published
+    }
+  }
+`
+
+export const EDIT_AUTHOR= gql`
+  mutation editAuthor( $name: String!, $born:Int!) {
+    editAuthor( name: $name, born:$born) {
+      name
+      born
+      id
+    }
+  }
+`
