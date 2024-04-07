799dc2f19281fead5e6c2853835d74c8912b8fe3
vibek44 <vibekkshetri@vibeks-MacBook-Pro.local>
2024-04-07 16:09:56 +0300

    using BrowserRouter for client side navigation, useQuery hook to fetch data, importing components required by app

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,42 @@
 import { useState } from 'react'
-
-
+import {
+  BrowserRouter as Router,
+  Routes, Route
+} from 'react-router-dom'
+import MenuLink from './components/MenuLink'
+import Home from './components/Home'
+import Authors from './components/Authors'
+import Books from './components/Books'
+import Notification from './components/Notification'
+import { useQuery } from '@apollo/client'
+import { ALL_AUTHORS } from './queries'
+import { ALL_BOOKS } from './queries'
 
 const App=() => {
+  const result =useQuery(ALL_AUTHORS)
+  const resultBooks=useQuery(ALL_BOOKS)
+  const [errorMessage ,setErrorMessage]=useState('')
 
+  const notify =( message ) => {
+    setErrorMessage(message)
+    setTimeout(() => {
+      setErrorMessage('')
+    },4000)
+  }
+  //console.log(resultBooks.data)
+  if(result.loading || resultBooks.loading)
+    return(<h3>...Loading</h3>)
   return (
-    <div>
-        initial setup
-    </div>
+    <Router>
+      <Notification message={errorMessage}/>
+      <h3>check available books | authors</h3>
+      <MenuLink/>
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/authors' element={<Authors authors={result.data.allAuthors } handleNotify={notify}/>}/>
+        <Route path='/books' element={<Books books={resultBooks.data.allBooks} handleNotify={notify}/>}/>
+      </Routes>
+    </Router>
   )
 }
 
