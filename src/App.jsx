d6d34aa8debbdba48114043c571a43496d6bafd5
vibek44 <vibekkshetri@vibeks-MacBook-Pro.local>
2024-04-17 18:30:58 +0300

    adding logout functionality,added routes to handle login and addBook

--- src/App.jsx
+++ src/App.jsx
@@ -1,42 +1,58 @@
 import { useState } from 'react'
 import {
-  BrowserRouter as Router,
-  Routes, Route
+  Routes, Route,
+  useNavigate
 } from 'react-router-dom'
 import MenuLink from './components/MenuLink'
 import Home from './components/Home'
 import Authors from './components/Authors'
 import Books from './components/Books'
 import Notification from './components/Notification'
-import { useQuery } from '@apollo/client'
-import { ALL_AUTHORS } from './queries'
-import { ALL_BOOKS } from './queries'
+import LoginForm from './components/LoginForm'
+import AddBookForm from './components/AddBookForm'
+import { useQuery,useApolloClient } from '@apollo/client'
+import { ALL_AUTHORS ,ALL_BOOKS,ME } from './queries'
+
 
 const App=() => {
   const result =useQuery(ALL_AUTHORS)
   const resultBooks=useQuery(ALL_BOOKS)
+  const resultUser=useQuery(ME)
+  const [token,setToken]=useState(null)
   const [errorMessage ,setErrorMessage]=useState('')
-
+  const navigate=useNavigate()
+  const client=useApolloClient()
+  //console.log(resultUser)
   const notify =( message ) => {
     setErrorMessage(message)
     setTimeout(() => {
       setErrorMessage('')
     },4000)
   }
-  //console.log(resultBooks.data)
+
+  const logout=() => {
+    setToken(null)
+    //localStorage.removeItem()
+    localStorage.clear()
+    client.resetStore()
+    navigate('/')
+  }
+  //console.log(result)
   if(result.loading || resultBooks.loading)
     return(<h3>...Loading</h3>)
   return (
-    <Router>
+    <div>
       <Notification message={errorMessage}/>
       <h3>check available books | authors</h3>
-      <MenuLink/>
+      <MenuLink token={token} logout={logout}/>
       <Routes>
-        <Route path='/' element={<Home/>}/>
-        <Route path='/authors' element={<Authors authors={result.data.allAuthors } handleNotify={notify}/>}/>
+        <Route path='/addbook' element={<AddBookForm handleNotify={notify}/>}/>
+        <Route path='/authors' element={<Authors authors={result.data.allAuthors } handleNotify={notify} token={token} />}/>
         <Route path='/books' element={<Books books={resultBooks.data.allBooks} handleNotify={notify}/>}/>
+        <Route path='/login' element={<LoginForm  handleNotify={ notify } setToken={setToken} token={token}/>}/>
+        <Route path='/' element={<Home/>}/>
       </Routes>
-    </Router>
+    </div>
   )
 }
 
