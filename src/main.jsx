0464e2c8a30d463aa5d06a7483cfdb9a8e53ad04
vibek44 <vibekkshetri@vibeks-MacBook-Pro.local>
2024-04-17 18:28:52 +0300

    importing and adding router for frontside routing,adding authLink to crate headers with token and wrapping uri into httpLink object

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,36 @@
-import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import { ApolloClient,ApolloProvider,InMemoryCache } from '@apollo/client'
+import { ApolloClient, ApolloProvider, InMemoryCache,createHttpLink } from '@apollo/client'
+import { setContext } from '@apollo/client/link/context'
+import {
+  BrowserRouter as Router
+} from 'react-router-dom'
 
-const client=new ApolloClient({
-  uri: 'http://localhost:4000/',
-  cache: new InMemoryCache(),
+
+const authLink=setContext((_, { headers }) => {
+  const token=localStorage.getItem('library-user-token',)
+  //console.log(token)
+  return{
+    headers:{
+      ...headers,
+      authorization:token ?`Bearer ${token}`:null
+    }
+  }
 })
 
+const httpLink=createHttpLink({
+  uri: 'http://localhost:4000/'
+})
+
+const client = new ApolloClient({
+  cache: new InMemoryCache(),
+  link:authLink.concat(httpLink),
+})
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <ApolloProvider client={client}>
-    <App />
+    <Router>
+      <App />
+    </Router>
   </ApolloProvider>
 )
