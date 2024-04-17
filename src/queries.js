59650d0588554d499c8b0d0fbdb423c35a8cdd9b
vibek44 <vibekkshetri@vibeks-MacBook-Pro.local>
2024-04-17 18:27:03 +0300

    adding author resolve field name and adding genres field in graphql types

--- src/queries.js
+++ src/queries.js
@@ -1,4 +1,14 @@
 import { gql } from '@apollo/client'
+
+export const ME = gql`
+query{
+  me{
+    id
+    username
+    favoriteGenre
+  }
+}`
+
 export const ALL_AUTHORS = gql`
 query{
   allAuthors{
@@ -11,17 +21,31 @@ query{
 export const ALL_BOOKS = gql`
 query{
   allBooks{
+    id
     title
-    author
+    author{
+      name
+    }
     published
+    genres
   }
 }`
+
+export const LOGIN = gql`
+  mutation login($username: String!, $password: String!) {
+    login(username: $username, password: $password)  {
+      value
+    }
+  }
+`
 
 export const ADD_BOOK = gql`
   mutation addBook($title: String!, $author: String!,$published:Int!,$genres:[String!]!) {
     addBook(title: $title, author: $author, published:$published, genres:$genres) {
       title
-      author
+      author{
+        name
+      }
       published
     }
   }
