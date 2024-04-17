cb9bcdc1c3453c27639a52864e9eed0c91cf3e3a
vibek44 <vibekkshetri@vibeks-MacBook-Pro.local>
2024-04-17 18:44:15 +0300

    using ternary conditional operator to manipulate navigation view for the user logged

--- src/components/MenuLink.jsx
+++ src/components/MenuLink.jsx
@@ -1,10 +1,17 @@
 import { Link } from 'react-router-dom'
 
-const MenuLink = () => {
+const MenuLink = ({ token,logout }) => {
+  const Style={
+    paddingRight:4
+  }
   return(<div>
-    <Link to='/' >Home</Link>
-    <Link to ='/authors'> Authors </Link>
-    <Link to='/books'> Books</Link>
+    <Link to='/' style={Style}>Home</Link>
+    <Link to ='/authors' style={Style}> Authors </Link>
+    <Link to='/books' style={Style}> Books</Link>
+    { token
+      ? <><Link to='/addbook'> addbook </Link> <button onClick={logout}>logout</button></>
+      :<Link to='/login' style={Style}>login</Link>
+    }
   </div>)
 }
 
