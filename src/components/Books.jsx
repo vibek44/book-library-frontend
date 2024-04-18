c1d8589a90d991628e0089629ab2934430e3bbd2
vibek44 <vibekkshetri@vibeks-MacBook-Pro.local>
2024-04-18 20:01:05 +0300

    filtering the books using graphql instead of react in frontend side and removing functionality from it

--- src/components/Books.jsx
+++ src/components/Books.jsx
@@ -1,16 +1,6 @@
-import { useState } from 'react'
 
-const Books=({ books }) => {
+const Books=({ books,handleGenre }) => {
   const genre=['science','philosophy','classics','drama','mystery','fantasy','medicine','all']
-  const [selectGenre,setSelectGenre]=useState(null)
-  let genreBooks=selectGenre?books.filter(book => book.genres.includes(selectGenre)):null
-  const handleGenre= ({ target }) => {
-    if(target.value==='all') {
-      setSelectGenre(null)
-      return
-    }
-    setSelectGenre(target.value)
-  }
   return(
     <div>
       <table>
@@ -20,17 +10,11 @@ const Books=({ books }) => {
             <td><b>author</b></td>
             <td><b>published</b></td>
           </tr>
-          { genreBooks
-            ? genreBooks.map(book => <tr key={book.title}>
-              <td>{book.title}</td>
-              <td>{book.author.name}</td>
-              <td>{book.published }</td>
-            </tr>)
-            :books.map(book => <tr key={book.title}>
-              <td>{book.title}</td>
-              <td>{book.author.name}</td>
-              <td>{book.published }</td>
-            </tr>)
+          { books.map(book => <tr key={book.title}>
+            <td>{book.title}</td>
+            <td>{book.author.name}</td>
+            <td>{book.published }</td>
+          </tr>)
           }
         </tbody>
       </table>
