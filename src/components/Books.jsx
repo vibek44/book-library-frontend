8e79f8e8f734f3e4df1516f5926bd34bb832045f
vibek44 <vibekkshetri@vibeks-MacBook-Pro.local>
2024-04-19 16:46:59 +0300

    using graphql Query to display or filter  data for specific user  action

--- src/components/Books.jsx
+++ src/components/Books.jsx
@@ -1,6 +1,21 @@
-
-const Books=({ books,handleGenre }) => {
+import { useState } from 'react'
+import { useQuery } from '@apollo/client'
+import { ALL_BOOKS } from '../queries'
+const Books=({ books }) => {
   const genre=['science','philosophy','classics','drama','mystery','fantasy','medicine','all']
+  const [selectGenre,setSelectGenre]=useState(null)
+  const genreBooks =useQuery(ALL_BOOKS,{
+    variables:{ selectGenre },
+    skip:!selectGenre   //variable here needs to match query def variable
+  })
+  const handleGenre= ({ target }) => {
+    if ( target.value==='all'){
+      setSelectGenre(null)
+      return
+    }
+    setSelectGenre(target.value)
+  }
+
   return(
     <div>
       <table>
@@ -10,11 +25,17 @@ const Books=({ books,handleGenre }) => {
             <td><b>author</b></td>
             <td><b>published</b></td>
           </tr>
-          { books.map(book => <tr key={book.title}>
-            <td>{book.title}</td>
-            <td>{book.author.name}</td>
-            <td>{book.published }</td>
-          </tr>)
+          { genreBooks.data
+            ?genreBooks.data.allBooks.map(book => <tr key={book.title}>
+              <td>{book.title}</td>
+              <td>{book.author.name}</td>
+              <td>{book.published }</td>
+            </tr>)
+            :books.map(book => <tr key={book.title}>
+              <td>{book.title}</td>
+              <td>{book.author.name}</td>
+              <td>{book.published }</td>
+            </tr>)
           }
         </tbody>
       </table>
