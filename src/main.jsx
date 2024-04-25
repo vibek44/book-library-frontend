d21ca0e37c81fae8941eb5cdc8a82258ff9bcebb
vibek44 <vibekkshetri@vibeks-MacBook-Pro.local>
2024-04-25 17:24:56 +0300

    adding link for websocket connection inaddition to http connection to facilitate subscription services for the app

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,10 @@
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import { ApolloClient, ApolloProvider, InMemoryCache,createHttpLink } from '@apollo/client'
+import { ApolloClient, ApolloProvider, InMemoryCache,createHttpLink, split } from '@apollo/client'
 import { setContext } from '@apollo/client/link/context'
+import { getMainDefinition } from '@apollo/client/utilities'
+import { GraphQLWsLink } from '@apollo/client/link/subscriptions'
+import { createClient } from 'graphql-ws'
 import {
   BrowserRouter as Router
 } from 'react-router-dom'
@@ -19,12 +22,30 @@ const authLink=setContext((_, { headers }) => {
 })
 
 const httpLink=createHttpLink({
-  uri: 'http://localhost:4000/'
+  uri: 'http://localhost:4000'
 })
+
+const wsLink=new GraphQLWsLink(
+  createClient({
+    url:'ws://localhost:4000'
+  })
+)
+
+const splitLink=split(
+  ({ query }) => {
+    const defination=getMainDefinition(query)
+    return(
+      defination.kind==='OperationDefinition' &&
+      defination.operation==='subscription'
+    )
+  },
+  wsLink,
+  authLink.concat(httpLink)
+)
 
 const client = new ApolloClient({
   cache: new InMemoryCache(),
-  link:authLink.concat(httpLink),
+  link:splitLink,
 })
 
 ReactDOM.createRoot(document.getElementById('root')).render(
